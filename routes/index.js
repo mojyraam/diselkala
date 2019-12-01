589426a0e535cc055c18c4f4540b8c4091e09d02
mojyraam <mojyraam@gmail.com>
2019-12-01 22:31:50 +0330

    v_1.0.4

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,21 @@ const { ensureAuthenticated } = require('../config/auth');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   if (req.user) {
-    res.render('index', {
-      title: 'ex',
-      user: req.user
-    })
+    if (req.user.usertype === 1) {
+      console.log('user')
+      res.render('index', {
+        title: 'ex',
+        user: req.user.usertype
+      })
+    } else if (req.user.usertype === 2) {
+      console.log('admin')
+      res.render('index', {
+        title: 'aex',
+        user: req.user.usertype
+      })
+    }
   } else {
+    console.log('guset')
     res.render('index', {
       title: 'express'
     })
